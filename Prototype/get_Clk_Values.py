ba42d8c182fe389c2285fc1ffdc99fda9d629972
BillyD <bdang@csu.fullerton.edu>
2017-09-25 12:21:55 -0700

    Perform an initial redesign of the code

--- Prototype/get_Clk_Values.py
+++ Prototype/get_Clk_Values.py
@@ -2,8 +2,12 @@
 import sys
 import os
 
+#board layout size
+p = 5
+m = 24
+n = 5
 
-class Process:
+class Package:
     #Private variables
     __message = None
     __time = None
@@ -27,9 +31,45 @@ class Process:
     def get_Time(self):
         return int(self.__time_Stamp)
 
+#empty class that 
+#class Process(Package P):
+    
+
+# populate function will help modified matrix values from default
+# hard code
+def populate():
+    #create and define process board
+    for i in range(p):
+        for j in range(2,n):
+            for k in range(m):
+                P_board = [i][j][k] = 0
+
+    P_board[0][0][0] = a
+    P_board[0][0][1] = b
+    P_board[0][0][2] = s1
+    P_board[0][0][3] = r3
+    P_board[0][0][4] = k
+    P_board[1][0][0] = a
+    P_board[0][0][0] = a
+    P_board[0][0][0] = a
+    P_board[0][0][0] = a
+    P_board[0][0][0] = a
+    P_board[0][0][0] = a
+    P_board[0][0][0] = a
+    P_board[0][0][0] = a
+    P_board[0][0][0] = a
+    P_board[0][0][0] = a
+
+
+
+
 # Sender is a helper function will get payload and send it to
 # designated target
-def sender(Process P, target):
+def sender(Process P):
+    for i in range(p):
+        for j in range(2,n):
+            for k in range(m):
+                if(P[i][j][k] == )
 
 
 
@@ -38,24 +78,22 @@ def sender(Process P, target):
 # payload for some process
 def recieve(Process P):
     (message,time_Stamp) = recieve()
-    time = max(time_Stamp, time)+1
+    time = max(time_Stamp, time) + 1
 
 
-
-
-# This function will help us increment the clock counter in a
+# First to be called, this function will help us increment the clock counter in a
 # a process locally before each event and send its payload to reciever.
 def loader_One(Process P):
     time = time + 1
     P.time_Stamp = 1
-    sender(P, target)
+    sender(P)
+
 
 
 def main():
-    # Create 3 processes
-    P1 = Process(" ", 0)
-    P2 = Process(" ", 0)
-    P3 = Process(" ", 0)
+
+    #initialize process board
+    populate()
 
     #Create events here
 
